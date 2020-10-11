539e53a858ef7126c0e2de7c240b9ee3e86c5390
Web-Cookie <webcookie1@gmail.com>
2020-10-12 00:05:41 +0200

    Flask

--- env_sample.py
+++ env_sample.py
@@ -3,5 +3,5 @@ import os
 os.environ.setdefault("IP", "0.0.0.0")
 os.environ.setdefault("PORT", "5000")
 os.environ.setdefault("SECRET_KEY", "your_secret_key_here")
-os.environ.setdefault("MONGO_URI", "")
+os.environ.setdefault("MONGO_URI", "mongodb+srv://John:Abc123@cluster0.kkcfz.mongodb.net/recipe_manager?retryWrites=true&w=majority")
 os.environ.setdefault("MONGO_DBNAME", "recipe_manager")
