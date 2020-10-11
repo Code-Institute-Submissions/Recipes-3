bd5f10608520a3e23fa04f1f1720fb68335b4693
Web-Cookie <webcookie1@gmail.com>
2020-10-11 22:44:00 +0200

    status not ok

--- app.py
+++ app.py
@@ -2,11 +2,14 @@ import os
 from flask import (Flask, render_template, redirect, request, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+if os.path.exists("env.py"):
+    import env
 
 app = Flask(__name__)
 
-app.config["MONGO_DBNAME"] = "recipe_manager"
-app.config["MONGO_URL"] = "mongodb+srv://John:Fuck123@cluster0.kkcfz.mongodb.net/recipe_manager?retryWrites=true&w=majority"
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
 
 mongo = PyMongo(app)
 
