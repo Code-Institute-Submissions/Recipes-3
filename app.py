4cc72b001c177d55bd648a97b2125c13d691df3e
Web-Cookie <webcookie1@gmail.com>
2020-10-12 20:25:53 +0200

    updating app.py

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 import os
-from flask import (Flask, render_template, redirect, request, url_for)
+from flask import (
+    Flask, flash, render_template, 
+    redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 if os.path.exists("env.py"):
     import env
+
 
 app = Flask(__name__)
 
@@ -12,6 +15,7 @@ app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 app.secret_key = os.environ.get("SECRET_KEY")
 
 mongo = PyMongo(app)
+
 
 @app.route("/")
 @app.route("/get_recipes")
